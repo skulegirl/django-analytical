5a67e9c0a95bb37de13de1deb145cdaefcf6410b
Anna Maste <skulegirlanna@gmail.com>
2017-10-21 12:12:42 -0400

    Switch to analytics.js - NOT FULLY TESTED

--- analytical/templatetags/google_analytics.py
+++ analytical/templatetags/google_analytics.py
@@ -30,31 +30,21 @@ PROPERTY_ID_RE = re.compile(r'^UA-\d+-\d+$')
 SETUP_CODE = """
     <script type="text/javascript">
 
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '%(property_id)s']);
-      _gaq.push(['_trackPageview']);
-      %(commands)s
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? %(source_scheme)s) + %(source_url)s;
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        %(commands)s
 
     </script>
 """
-DOMAIN_CODE = "_gaq.push(['_setDomainName', '%s']);"
-NO_ALLOW_HASH_CODE = "_gaq.push(['_setAllowHash', false]);"
-ALLOW_LINKER_CODE = "_gaq.push(['_setAllowLinker', true]);"
-CUSTOM_VAR_CODE = "_gaq.push(['_setCustomVar', %(index)s, '%(name)s', " \
-                  "'%(value)s', %(scope)s]);"
-SITE_SPEED_CODE = "_gaq.push(['_trackPageLoadTime']);"
-ANONYMIZE_IP_CODE = "_gaq.push (['_gat._anonymizeIp']);"
-SAMPLE_RATE_CODE = "_gaq.push (['_setSampleRate', '%s']);"
-SITE_SPEED_SAMPLE_RATE_CODE = "_gaq.push (['_setSiteSpeedSampleRate', '%s']);"
-SESSION_COOKIE_TIMEOUT_CODE = "_gaq.push (['_setSessionCookieTimeout', '%s']);"
-VISITOR_COOKIE_TIMEOUT_CODE = "_gaq.push (['_setVisitorCookieTimeout', '%s']);"
-DEFAULT_SOURCE = ("'https://ssl' : 'http://www'", "'.google-analytics.com/ga.js'")
-DISPLAY_ADVERTISING_SOURCE = ("'https://' : 'http://'", "'stats.g.doubleclick.net/dc.js'")
+CREATE_CODE = "ga('create', '%(property_id)s', 'auto' %(additional_args)s);"
+SEND_PAGEVIEW_CODE = "ga('send', 'pageview');"
+DOMAIN_CODE = "ga('linker:autoLink', ['%s']);"
+REQUIRE_LINKER_CODE = "ga('require', 'linker');"
+CUSTOM_VAR_CODE = "ga('set', '%(name)s', '%(value)s');"
+ANONYMIZE_IP_CODE = "ga('set', 'anonymizeIp', true);"
 
 ZEROPLACES = decimal.Decimal('0')
 TWOPLACES = decimal.Decimal('0.01')
@@ -87,15 +77,8 @@ class GoogleAnalyticsNode(Node):
         commands = self._get_domain_commands(context)
         commands.extend(self._get_custom_var_commands(context))
         commands.extend(self._get_other_commands(context))
-        if getattr(settings, 'GOOGLE_ANALYTICS_DISPLAY_ADVERTISING', False):
-            source = DISPLAY_ADVERTISING_SOURCE
-        else:
-            source = DEFAULT_SOURCE
         html = SETUP_CODE % {
-            'property_id': self.property_id,
             'commands': " ".join(commands),
-            'source_scheme': source[0],
-            'source_url': source[1],
         }
         if is_internal_ip(context, 'GOOGLE_ANALYTICS'):
             html = disable_html(html, 'Google Analytics')
@@ -105,54 +88,35 @@ class GoogleAnalyticsNode(Node):
         commands = []
         tracking_type = getattr(settings, 'GOOGLE_ANALYTICS_TRACKING_STYLE',
                                 TRACK_SINGLE_DOMAIN)
-        if tracking_type == TRACK_SINGLE_DOMAIN:
-            pass
-        else:
+        commands.append(self._get_create_command(context, tracking_type))
+        if tracking_type != TRACK_SINGLE_DOMAIN:
+            commands.append(REQUIRE_LINKER_CODE)
             domain = get_domain(context, 'google_analytics')
             if domain is None:
                 raise AnalyticalException(
                     "tracking multiple domains with Google Analytics requires a domain name")
             commands.append(DOMAIN_CODE % domain)
-            commands.append(NO_ALLOW_HASH_CODE)
-            if tracking_type == TRACK_MULTIPLE_DOMAINS:
-                commands.append(ALLOW_LINKER_CODE)
+        commands.append(SEND_PAGEVIEW_CODE)
         return commands
 
-    def _get_custom_var_commands(self, context):
-        values = (
-            context.get('google_analytics_var%s' % i) for i in range(1, 6)
-        )
-        params = [(i, v) for i, v in enumerate(values, 1) if v is not None]
-        commands = []
-        for index, var in params:
-            name = var[0]
-            value = var[1]
-            try:
-                scope = var[2]
-            except IndexError:
-                scope = SCOPE_PAGE
-            commands.append(CUSTOM_VAR_CODE % {
-                'index': index,
-                'name': name,
-                'value': value,
-                'scope': scope,
-            })
-        return commands
-
-    def _get_other_commands(self, context):
-        commands = []
-        if getattr(settings, 'GOOGLE_ANALYTICS_SITE_SPEED', False):
-            commands.append(SITE_SPEED_CODE)
-
-        if getattr(settings, 'GOOGLE_ANALYTICS_ANONYMIZE_IP', False):
-            commands.append(ANONYMIZE_IP_CODE)
+    def _get_create_command(self, context, tracking_type):
+        argstrings = []
+        if tracking_type != TRACK_SINGLE_DOMAIN:
+            argstrings.append("'allowLinker': true")
 
         sampleRate = getattr(settings, 'GOOGLE_ANALYTICS_SAMPLE_RATE', False)
         if sampleRate is not False:
             value = decimal.Decimal(sampleRate)
             if not 0 <= value <= 100:
                 raise AnalyticalException("'GOOGLE_ANALYTICS_SAMPLE_RATE' must be >= 0 and <= 100")
-            commands.append(SAMPLE_RATE_CODE % value.quantize(TWOPLACES))
+            argstrings.append("'sampleRate' : %s" % value.quantize(TWOPLACES))
+
+        sessionCookieTimeout = getattr(settings, 'GOOGLE_ANALYTICS_SESSION_COOKIE_TIMEOUT_SECONDS', False)
+        if sessionCookieTimeout is not False:
+            value = decimal.Decimal(sessionCookieTimeout)
+            if value < 0:
+                raise AnalyticalException("'GOOGLE_ANALYTICS_SESSION_COOKIE_TIMEOUT_SECONDS' must be >= 0")
+            argstrings.append("'cookieExpires' : %s" % value.quantize(ZEROPLACES))
 
         siteSpeedSampleRate = getattr(settings, 'GOOGLE_ANALYTICS_SITE_SPEED_SAMPLE_RATE', False)
         if siteSpeedSampleRate is not False:
@@ -160,21 +124,38 @@ class GoogleAnalyticsNode(Node):
             if not 0 <= value <= 100:
                 raise AnalyticalException(
                     "'GOOGLE_ANALYTICS_SITE_SPEED_SAMPLE_RATE' must be >= 0 and <= 100")
-            commands.append(SITE_SPEED_SAMPLE_RATE_CODE % value.quantize(TWOPLACES))
+            argstrings.append("'siteSpeedSampleRate' : %s" % value.quantize(TWOPLACES))
 
-        sessionCookieTimeout = getattr(settings, 'GOOGLE_ANALYTICS_SESSION_COOKIE_TIMEOUT', False)
-        if sessionCookieTimeout is not False:
-            value = decimal.Decimal(sessionCookieTimeout)
-            if value < 0:
-                raise AnalyticalException("'GOOGLE_ANALYTICS_SESSION_COOKIE_TIMEOUT' must be >= 0")
-            commands.append(SESSION_COOKIE_TIMEOUT_CODE % value.quantize(ZEROPLACES))
+        argstring = ','.join(argstrings)
+        additional_args = ""
+        if argstring:
+            additional_args = ", " + "{" + argstring + "}"
+        command = CREATE_CODE % {
+            'property_id': self.property_id,
+            'additional_args': additional_args,
+        }
+        return command
 
-        visitorCookieTimeout = getattr(settings, 'GOOGLE_ANALYTICS_VISITOR_COOKIE_TIMEOUT', False)
-        if visitorCookieTimeout is not False:
-            value = decimal.Decimal(visitorCookieTimeout)
-            if value < 0:
-                raise AnalyticalException("'GOOGLE_ANALYTICS_VISITOR_COOKIE_TIMEOUT' must be >= 0")
-            commands.append(VISITOR_COOKIE_TIMEOUT_CODE % value.quantize(ZEROPLACES))
+    def _get_custom_var_commands(self, context):
+        values = (
+            context.get('google_analytics_var%s' % i) for i in range(1, 10)
+        )
+        params = [(i, v) for i, v in enumerate(values, 1) if v is not None]
+        commands = []
+        for index, var in params:
+            name = var[0]
+            value = var[1]
+            commands.append(CUSTOM_VAR_CODE % {
+                'name': name,
+                'value': value,
+            })
+        return commands
+
+    def _get_other_commands(self, context):
+        commands = []
+        if getattr(settings, 'GOOGLE_ANALYTICS_ANONYMIZE_IP', False):
+            commands.append(ANONYMIZE_IP_CODE)
+
         return commands
 
 
